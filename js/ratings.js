8ceb25dcb00956ae19703a787b3f9d2dcca50b03
malyrudy <malyrudy@gmail.com>
2022-05-31 21:18:33 +0200

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -10,7 +10,18 @@ const collect_ratings = () => {
     elements.forEach(element => {
         rating = parseInt(element.id.replace("star",""))
         ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
 
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
 
 }
+
+addEventListener("change", () => {
+    const ratings = collect_ratings();
+    document.querySelectorAll("#average").value = ratings.average.toFixed(2);
+});
